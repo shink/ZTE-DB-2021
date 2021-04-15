84fe876c215ecbc85ad7157772d04ac16e566ade
tsund <tsund@qq.com>
2021-04-15 12:43:42 +0800

    init

---
+++ preliminary/DB-2021/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * @author: shenke
+ * @date: 2021/4/14
+ * @project: ZTE_DB_2021
+ * @desp:
+ */
+
+#define TEST
+
+#include <bits/stdc++.h>
+
+
+int main() {
+    std::cout << "hello" << std::endl;
+    exit(0);
+}
---
+++ preliminary/shell/build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+cd $BASEDIR
+cd ..
+
+if [ ! -d DB-2021 ]
+then
+    echo "ERROR: $BASEDIR is not a valid directory of DB-2021."
+    exit -1
+fi
+cmake --version 2>&1
+tmp=$?
+if [ ${tmp} -ne 0 ]
+then
+    echo "ERROR: You should install cmake(2.8 or later) first."
+    echo "  Please goto https://cmake.org to download and install it."
+    exit
+fi
+
+rm -rf bin
+mkdir bin
+rm -rf build
+mkdir build
+
+sed -i "s/^#define TEST/\/\/ #define TEST/" DB-2021/Statement.h
+
+cd build
+cmake ../DB-2021
+tmp=$?
+echo "cmake return:" ${tmp}
+if [ ${tmp} -ne 0 ]
+then
+ echo "cmake <>return:" ${tmp}
+ exit -1
+fi
+
+make
+tmp=$?
+ echo "make return:" ${tmp}
+if [ ${tmp} -ne 0 ]
+then
+echo "make <>return:" ${tmp}
+ exit -1
+fi
+
+cd ..
+sed -i "s/^\/\/ \#define TEST/\#define TEST/" DB-2021/Statement.h
